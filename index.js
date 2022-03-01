8bd5cb452dad1a454f855225062e376537f05ab0
Daniel Stephen <daniel.vergil2711@gmail.com>
2022-02-28 22:21:46 -0800

    Completed inquirer prompts and employee classes

--- index.js
+++ index.js
@@ -2,30 +2,31 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const path = require('path');
-const generate = require('./utils/generateMarkdown');
+const generate = require('./src/generateHtml');
 
 // Create an array of questions for user input
 const questions = [
-    "Enter your team member's name:",    
+    "Enter your team member's name:",
     "Select your team member's role:",
-    "Enter your team member's id:", 
+    "Enter your team member's id:",
     "Enter your team member's email address:",
     "Enter your team member's GitHub username:",
     "Enter your team member's School name:",
-    "Enter your team member's office number:"
+    "Enter your team member's office number:",
+    "Would you like to add more team members?"
 ];
 
 // Create a function to write HTML file
 function writeToFile(fileName, data) {
     try {
-    return fs.writeFileSync(path.join(process.env.PWD, '/dist/', fileName), generate(data));
+        return fs.writeFileSync(path.join(process.env.PWD, '/dist/', fileName), generate(data));
     } finally {
-        console.log("ReadMe file is generated. Please check the output folder. Thank you!");
+        console.log("Team profile HTML is generated. Please check the dist folder. Thank you!");
     }
 }
 
 // Display instructions to the users
-console.log("INSTRUCTIONS: Please answer the below prompts to generate a high quality README file. Readme file will be auto-generated in the output folder. If you want to enter multi-line answers, please include <br> tag at the end of each line. Thank you!");
+console.log("INSTRUCTIONS: Please answer the below prompts to generate a your team profile. Team profile HTML file will be auto-generated in the dist/ folder. Thank you!");
 
 // Create a function to initialize app
 function init() {
@@ -33,73 +34,62 @@ function init() {
         .prompt([{
                 type: "input",
                 message: questions[0],
-                name: "title",
+                name: "name",
             },
             {
-                type: "input",
+                type: "list",
                 message: questions[1],
-                name: "descriptionMotivation",
+                choices: ["Manager", new inquirer.Separator(), "Engineer", new inquirer.Separator(), "Intern"],
+                name: "role",
             },
             {
                 type: "input",
                 message: questions[2],
-                name: "descriptionReason",
+                name: "id",
             },
             {
-                type: "input",
+                type: "email",
                 message: questions[3],
-                name: "descriptionSolution",
-            },
-            {
-                type: "input",
-                message: questions[4],
-                name: "descriptionLearning",
-            },
-            {
-                type: "input",
-                message: questions[5],
-                name: "installation",
-            },
-            {
-                type: "input",
-                message: questions[6],
-                name: "usage",
-            },
-            {
-                type: "input",
-                message: questions[7],
-                name: "credits",
-            },
-            {
-                type: "checkbox",
-                message: questions[8],
-                choices: ["MIT", "GNU General Public License 2.0", "Apache License 2.0", "GNU General Public License 3.0", "none"],
-                name: "license",
-            },
-            {
-                type: "input",
-                message: questions[9],
-                name: "contribution",
-            },
-            {
-                type: "input",
-                message: questions[10],
-                name: "testingInstructions"
-            },
-            {
-                type: "input",
-                message: questions[11],
-                name: "githubUname"
-            },
-            {
-                type: "input",
-                message: questions[12],
-                name: "email"
-            },
+                name: "email",
+            }
         ])
         .then((response) => {
-            writeToFile("README.md", response);
-        });
+            // writeToFile("README.md", response);
+            if (response.role == "Manager") {
+                inquirer
+                    .prompt([{
+                        type: "input",
+                        message: questions[6],
+                        name: "officeNumber",
+                    }]).then(response => addProfile());
+            } else if (response.role == "Engineer") {
+                inquirer
+                    .prompt([{
+                        type: "input",
+                        message: questions[4],
+                        name: "githubUserName",
+                    }]).then(response => addProfile());
+            } else {
+                inquirer
+                    .prompt([{
+                        type: "input",
+                        message: questions[5],
+                        name: "schoolName",
+                    }]).then(response => addProfile());
+            }
+        })
+    }
+
+function addProfile() {
+    inquirer.prompt([{
+        type: "confirm",
+        message: questions[7],
+        name: "addMember"
+    }]).then(response => {
+        if (response.addMember) {
+            init();
+        }
+    })
 }
 
 // Function call to initialize app
